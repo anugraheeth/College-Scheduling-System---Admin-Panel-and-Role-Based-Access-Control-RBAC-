706c8d1d9f91cc63914c2c23d8792bb7e0d285da
Anugraheeth Mohanan <161076398+anugraheeth@users.noreply.github.com>
2024-12-03 23:30:11 +0530

    Update db.js

--- backend/Utils/db.js
+++ backend/Utils/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const DbCon = async()=>{
     try {
-        await mongoose.connect(process.env.MONGODB)
+        await mongoose.connect('mongodb+srv://mydatabase:YoNCBQbpxY3SGMm6@cluster0.5lad2.mongodb.net/RBAC?retryWrites=true&w=majority&appName=Cluster0')
         console.log("MongoDB is connected")
     } catch (error) {
         console.log(error)
